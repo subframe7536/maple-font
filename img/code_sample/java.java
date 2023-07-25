c8bdf5edbbc65586c240798f5c0104214673ee2a
subframe7536 <1667077010@qq.com>
2023-07-25 08:56:07 +0800

    improve java code example

--- img/code_sample/java.java
+++ img/code_sample/java.java
@@ -1,10 +1,20 @@
+import java.util.List;
+
 public class test {
-    public static void main(String[] args){
-        List<Integer> transactionsIds = widgets.stream()
-            .filter(b -> b.getColor() == RED)
-            .sorted((x,y) -> x.getWeight() - y.getWeight())
-            .mapToInt(Widget::getWeight)
-            .sum();
-        System.out.println(transactionsIds);
+  private static final int RED = 0;
+
+  class Widget {
+    public int getWeight() {
+      return 0;
     }
+  }
+
+  public static void main(String[] args) {
+    List<Integer> transactionsIds = widgets.stream()
+        .filter(b -> b.getColor() == test.RED)
+        .sorted((x, y) -> x.getWeight() - y.getWeight())
+        .mapToInt(Widget::getWeight)
+        .sum();
+    System.out.println(transactionsIds);
+  }
 }
