f69e8cb24e0a42582400a4412b9ba81dda71dc93
subframe7536 <1667077010@qq.com>
2023-07-29 01:05:05 +0800

    重命名配置文件并修正中文构建脚本

--- source/generate-sc.bat
+++ source/generate-sc.bat
@@ -5,8 +5,6 @@ set python_exe="C:\Program Files (x86)\FontForgeBuilds\bin\ffpython.exe"
 @REM available: ttf / ttf-autohint / nf
 set base_font="nf"
 
-python ./build.py
-
 %python_exe% ./SC/merge.py %base_font%
 
 python ./SC/fix.py %base_font%
