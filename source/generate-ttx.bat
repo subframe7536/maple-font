34cea74128b3e5bd72f4ba6efa73cd895d758d99
subframe7536 <1667077010@qq.com>
2023-07-29 10:32:16 +0800

    fix ttx generate script

--- source/generate-ttx.bat
+++ source/generate-ttx.bat
@@ -10,9 +10,9 @@ if not exist %dir% (
 
 if exist .\ttx rmdir /s /q .\ttx
 
-ttx -f -d .\ttx\%name%-Regular .\%dir%\%name%-Regular.ttf
-ttx -f -d .\ttx\%name%-Bold .\%dir%\%name%-Bold.ttf
-ttx -f -d .\ttx\%name%-Light .\%dir%\%name%-Light.ttf
-ttx -f -d .\ttx\%name%-Italic .\%dir%\%name%-Italic.ttf
-ttx -f -d .\ttx\%name%-BoldItalic .\%dir%\%name%-BoldItalic.ttf
-ttx -f -d .\ttx\%name%-LightItalic .\%dir%\%name%-LightItalic.ttf
+ttx -s -f -d .\ttx\%name%-Regular .\%dir%\%name%-Regular.ttf
+ttx -s -f -d .\ttx\%name%-Bold .\%dir%\%name%-Bold.ttf
+ttx -s -f -d .\ttx\%name%-Light .\%dir%\%name%-Light.ttf
+ttx -s -f -d .\ttx\%name%-Italic .\%dir%\%name%-Italic.ttf
+ttx -s -f -d .\ttx\%name%-BoldItalic .\%dir%\%name%-BoldItalic.ttf
+ttx -s -f -d .\ttx\%name%-LightItalic .\%dir%\%name%-LightItalic.ttf
