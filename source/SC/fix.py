d1df695abf70ff4e9e8a8de00d24d1c43dc47b4a
subframe7536 <1667077010@qq.com>
2023-10-09 18:27:54 +0800

    添加是否修复编码选项

--- source/SC/fix.py
+++ source/SC/fix.py
@@ -3,13 +3,15 @@ from fontTools.ttLib import TTFont, newTable
 from os import getcwd, path, listdir
 
 
-if len(sys.argv) < 3:
-    print("配置不正确，将使用默认的 'nf' 和 'Maple Mono'")
+if len(sys.argv) < 4:
+    print("配置不正确，将使用默认配置")
     base_font = "nf"
+    fix_encoding_and_meta = True
     family_name = "Maple Mono"
 else:
     base_font = sys.argv[1]
-    family_name = sys.argv[2]
+    fix_encoding_and_meta = sys.argv[2] == "1"
+    family_name = sys.argv[3]
 
 suffix = "SC-NF" if base_font == "nf" else "SC"
 suffix_alt = suffix.replace("-", " ")
@@ -45,16 +47,17 @@ for f in listdir(ttx_path):
 
     font.importXML(path.join(ttx_path, f, f + ".O_S_2f_2.ttx"))
 
-    # add code page, Latin / Japanese / Simplify Chinese / Traditional Chinese
-    font["OS/2"].ulCodePageRange1 = 1 << 0 | 1 << 17 | 1 << 18 | 1 << 20
+    if fix_encoding_and_meta:
+        # add code page, Latin / Japanese / Simplify Chinese / Traditional Chinese
+        font["OS/2"].ulCodePageRange1 = 1 << 0 | 1 << 17 | 1 << 18 | 1 << 20
 
-    # fix meta table, https://learn.microsoft.com/en-us/typography/opentype/spec/meta
-    meta = newTable("meta")
-    meta.data = {
-        "dlng": "Latn, Hans, Hant, Jpan",
-        "slng": "Latn, Hans, Hant, Jpan",
-    }
-    font["meta"] = meta
+        # fix meta table, https://learn.microsoft.com/en-us/typography/opentype/spec/meta
+        meta = newTable("meta")
+        meta.data = {
+            "dlng": "Latn, Hans, Hant, Jpan",
+            "slng": "Latn, Hans, Hant, Jpan",
+        }
+        font["meta"] = meta
 
     font.save(target)
     font.close()
--- source/generate-sc.bat
+++ source/generate-sc.bat
@@ -3,10 +3,15 @@
 set python_forge_exe="C:\Program Files (x86)\FontForgeBuilds\bin\ffpython.exe"
 
 @REM available: ttf / ttf-autohint / nf
+@REM 构建的基准字体
 set base_font="nf"
+
+@REM available: 0 / 1
+@REM 是否修复编码和元数据，出现乱码时可尝试切换
+set fix_encoding_and_meta=1
 
 %python_forge_exe% ./SC/merge.py %base_font% %1
 
 if %ERRORLEVEL% equ 0 (
-    python ./SC/fix.py %base_font% %1
+    python ./SC/fix.py %base_font% %fix_encoding_and_meta% %1
 )
