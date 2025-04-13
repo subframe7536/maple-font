d99db38cbf758165f23cf06250e6dcfe84fecd79
subframe7536 <1667077010@qq.com>
2025-04-13 22:09:20 +0800

    add missing `i_l.liga.cv33.ss06`

--- source/py/feature/ss/ss06.py
+++ source/py/feature/ss/ss06.py
@@ -9,6 +9,7 @@ def ss06_subst():
             ast.gly("cl"),
             ast.gly("el"),
             ast.gly("il"),
+            ast.gly("il", ".cv33"),
             ast.gly("ll"),
             ast.gly("tl"),
             ast.gly("ul"),
