454c05bbe342df267e741ce9f2f8033e82d0fc17
Song <1667077010@qq.com>
2025-04-27 19:34:02 +0800

    Update source/py/feature/base/locl.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- source/py/feature/base/locl.py
+++ source/py/feature/base/locl.py
@@ -99,8 +99,5 @@ def get_locl_feature_list(cn: bool, cn_only: bool = False):
     if not cn:
         return [ast.Feature("locl", __locl, "7.0")]
 
-    if cn_only:
-        content = __locl_cn_only
-    else:
-        content = __locl + __locl_cn_only
+    content = __locl_cn_only if cn_only else __locl + __locl_cn_only
     return [lookup_tw, ast.Feature("locl", content, "7.0")]
