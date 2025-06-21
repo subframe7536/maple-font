b235f664a16e8e75527f82f97899a015d0fcabe3
subframe7536 <1667077010@qq.com>
2025-06-21 21:19:46 +0800

    add more glyphs in cv36 for cyrillic symbols #532

--- source/py/feature/cv/cv36.py
+++ source/py/feature/cv/cv36.py
@@ -3,7 +3,16 @@ import source.py.feature.ast as ast
 
 def cv36_subst():
     return ast.subst_map(
-        ["x", ast.gly("xl"), ast.gly("xl", ".cv04"), ast.gly("xl", ".cv35")],
+        [
+            "x",
+            "ha-cy",
+            "hadescender-cy",
+            "hahook-cy",
+            "hastroke-cy",
+            ast.gly("xl"),
+            ast.gly("xl", ".cv04"),
+            ast.gly("xl", ".cv35"),
+        ],
         target_suffix=".cv36",
     )
 
